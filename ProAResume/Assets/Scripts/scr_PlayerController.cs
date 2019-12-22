cd7c80b1872d1ef97363fe07f3c764192802691b
ChrisCrossed <chriscrossed.86@gmail.com>
2019-12-21 22:41:16 -0800

    Checkpoint - Pre-XInput Submission

---
+++ ProAResume/Assets/Scripts/scr_PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Player Requirements
+[RequireComponent(typeof(Rigidbody))]
+
+public class scr_PlayerController : scr_PlayerInput
+{
+    GameObject this_Player;
+    Rigidbody this_RigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this_Player = gameObject;
+        this_RigidBody = this_Player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProAResume/Assets/Scripts/scr_PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PlayerInput
+{
+    // Game Settings
+    private float _f_zDir; // Forward or Backward
+    private float _f_xDir; // Strafe Left or Right
+    private float _f_LookHoriz;
+    private float _f_LookVert;
+    private bool _b_JumpPressed;
+    private bool _b_JumpHeld;
+    private bool _b_DPad_LeftPressed;
+    private bool _b_DPad_RightPressed;
+    private bool _b_DPad_UpPressed;
+    private bool _b_DPad_DownPressed;
+}
+
+public class scr_PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
