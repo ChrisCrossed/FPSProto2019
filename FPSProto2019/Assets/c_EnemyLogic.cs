f8ac3dbce16f79d09d9dd201e241fd1d8208be43
ChrisCrossed <chriscrossed.86@gmail.com>
2023-06-28 00:25:00 -0700

    Starting AI Pathing Logic
    Changing logic to move pathnodes to a Level Logic script that will contain all pathnodes and their respective pathing behavior

--- FPSProto2019/Assets/c_EnemyLogic.cs
+++ FPSProto2019/Assets/c_EnemyLogic.cs
@@ -3,27 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum EnemyStance
+{
+    Passive,
+    Investigating,
+    Hunting,
+    Fighting,
+    Dead
+}
+
+
+
 [RequireComponent(typeof(NavMeshAgent))]
+
 
 public class c_EnemyLogic : MonoBehaviour
 {
     NavMeshAgent NavMeshAgent;
+
+    EnemyStance EnemyStance;
 
     // Start is called before the first frame update
     void Start()
     {
         NavMeshAgent = gameObject.GetComponent<NavMeshAgent>();
 
-        GameObject go_GoalNode = GameObject.Find("TempGoal");
+        // GameObject go_GoalNode = GameObject.Find("TempGoal");
+        List<GameObject> go_GoalNodes = new List<GameObject>();
 
-        SetGoalPosition(go_GoalNode.transform.position);
+        foreach(GameObject pathnode in GameObject.FindGameObjectsWithTag("PathNode"))
+        {
+            go_GoalNodes.Add(pathnode);
+        }
 
+        SetGoalPosition(go_GoalNodes[0]);
     }
 
+    void SetEnemyStance(GameObject target)
+    {
+        switch (EnemyStance)
+        {
+            case EnemyStance.Passive:
+                break;
+            case EnemyStance.Investigating:
+                SetGoalPosition(target.transform.position);
+                break;
+            case EnemyStance.Hunting:
+                SetGoalPosition(target.transform.position);
+                break;
+            case EnemyStance.Fighting:
+                SetGoalPosition(target.transform.position);
+                break;
+            case EnemyStance.Dead:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void SetEnemyStance()
+    {
+        switch (EnemyStance)
+        {
+            case EnemyStance.Passive:
+                break;
+            case EnemyStance.Investigating:
+                break;
+            case EnemyStance.Hunting:
+                break;
+            case EnemyStance.Fighting:
+                break;
+            case EnemyStance.Dead:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void SetGoalPosition(GameObject go_GoalPosition)
+    {
+        SetGoalPosition(go_GoalPosition.transform.position);
+    }
     void SetGoalPosition(Vector3 v3_GoalPosition)
     {
         NavMeshAgent.destination = v3_GoalPosition;
     }
+
+    
 
     // Update is called once per frame
     void Update()
---
+++ FPSProto2019/Assets/c_LevelLogic.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c_LevelLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    List<Vector3> GoalNodes = new List<Vector3>();
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
